cb171ba69d5219220a2dc2d9b46c9e556aa1e1a3
Sinkyo <yhko1988@gmail.com>
2022-04-02 17:03:37 +0900

    :seedling::SEED: Init dashboard and notfound page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/Login'));
 const Signup = lazy(() => import('./pages/Signup'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
 
 function App() {
   return (
     <Suspense fallback={<p>...Loading</p>}>
       <Routes>
         <Route path={ROUTES.LOGIN} element={<Login />} />
-        <Route path={ROUTES.DASHBOARD} element={<div>DASHBOARD</div>} />
+        <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
         <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+        <Route path={ROUTES.NOT_FOUNT} element={<NotFound />} />
+        <Route path={'*'} element={<NotFound />} />
       </Routes>
     </Suspense>
   );
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div>Dash board</div>;
+}
+
+export default Dashboard;
