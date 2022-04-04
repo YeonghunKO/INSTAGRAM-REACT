09e653bdedbe1493ffbdde20829f9b4201fcbe11
Sinkyo <yhko1988@gmail.com>
2022-04-04 18:20:32 +0900

    :sparkles::NEW FEAT: For the rest of work for listening to loggedin user

---
+++ src/constants/path.js
@@ -0,0 +1,5 @@
+const DEFAULT_IMAGE_PATH = '/images/avatars/default.png';
+const INSTAGRAM_LOGO = '/images/logo.png';
+const USER_PROFILE_IMAGE = username => `images/avatars/${username}.jpg`;
+
+export { DEFAULT_IMAGE_PATH, INSTAGRAM_LOGO, USER_PROFILE_IMAGE };
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,6 +2,6 @@ const DASHBOARD = '/';
 const LOGIN = '/login';
 const SIGN_UP = '/sign-up';
 const PROFILE = '/p:username';
-const NOT_FOUNT = '/not-found';
+const NOT_FOUNT = '/*';
 
 export { DASHBOARD, LOGIN, SIGN_UP, PROFILE, NOT_FOUNT };
