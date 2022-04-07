0f6ced02fb4b45c1f13f7c6fb2fd300fc0ece928
Sinkyo <yhko1988@gmail.com>
2022-04-07 11:08:45 +0900

    :arrow_up:UPGRADE DEP: Change skeleton dependency

--- src/components/sidebar/User.js
+++ src/components/sidebar/User.js
@@ -1,51 +1,37 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import ContentLoader from 'react-content-loader';
 import { DEFAULT_IMAGE_PATH } from '../../constants/path';
 
 // react-content-loader로 해봐라 그럼 위치를 조정가능
 
 function User({ username, fullName, photoURL }) {
-  // return !username || !fullName ? (
-  //   <Skeleton count={1} height={61} />
-  // ) : (
-  //   <Link
-  //     to={`/p/${username}`}
-  //     className="grid grid-cols-4 gpa-4 mb-6 items-center"
-  //   >
-  //     <div className="flex items-center justify-center col-span-1">
-  //       <img
-  //         className="rounded-full w-13 flex mr-3"
-  //         src={photoURL}
-  //         alt="username"
-  //         onError={e => {
-  //           e.target.src = DEFAULT_IMAGE_PATH;
-  //         }}
-  //       />
-  //     </div>
-  //     <div className="col-span-3">
-  //       <p className="font-bold text-sm">{username}</p>
-  //       <p className="text-sm">{fullName}</p>
-  //     </div>
-  //   </Link>
-  // );
-  return (
-    <>
-      <Skeleton
-        className="mr-3"
-        circle
-        height="100%"
-        width="20%"
-        inline={true}
-      />
-      <Skeleton
-        className="self-center"
-        height="50%"
-        width="20%"
-        inline={true}
-      />
-    </>
+  return !username || !fullName ? (
+    <ContentLoader viewBox="0 0 380 70">
+      <circle cx="30" cy="30" r="30" />
+      <rect x="80" y="17" rx="4" ry="4" width="100" height="13" />
+      <rect x="80" y="40" rx="3" ry="3" width="150" height="10" />
+    </ContentLoader>
+  ) : (
+    <Link
+      to={`/p/${username}`}
+      className="grid grid-cols-4 gpa-4 mb-6 items-center"
+    >
+      <div className="flex items-center justify-center col-span-1">
+        <img
+          className="rounded-full w-12 h-12 flex mr-3"
+          src={photoURL}
+          alt="username"
+          onError={e => {
+            e.target.src = DEFAULT_IMAGE_PATH;
+          }}
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
   );
 }
 
