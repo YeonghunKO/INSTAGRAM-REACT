8e17fe3ddccf2b0bd1c556178aaf1bdd67e21aaf
Sinkyo <yhko1988@gmail.com>
2022-04-13 16:13:13 +0900

    :sparkles::DONE: Done protectedRoute for Dashboard

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import * as ROUTES from './constants/routes';
 import UserContext from './context/currentUser';
 import useAuthListner from './hooks/useAuthListener';
 
+import ProtectedRoute from './helpers/ProtectedRoute';
+
 const Login = lazy(() => import('./pages/Login'));
 const Signup = lazy(() => import('./pages/Signup'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Profile = lazy(() => import('./pages/Profile'));
 
 function App() {
   const { user } = useAuthListner();
@@ -15,8 +18,16 @@ function App() {
     <UserContext.Provider value={{ user }}>
       <Suspense fallback={<p>...Loading</p>}>
         <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          <Route
+            path={ROUTES.DASHBOARD}
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          ;
+          <Route path={ROUTES.LOGIN} element={<Login />} />;
           <Route path={ROUTES.SIGN_UP} element={<Signup />} />
           <Route path={ROUTES.NOT_FOUNT} element={<NotFound />} />
         </Routes>
---
+++ src/helpers/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+
+function ProtectedRoute({ user, children }) {
+  if (!user) {
+    return <Navigate to={ROUTES.LOGIN} replace />;
+  }
+  return children;
+}
+
+export default ProtectedRoute;
+
+ProtectedRoute.propTypes = {
+  user: PropTypes.object,
+  children: PropTypes.object.isRequired,
+};
