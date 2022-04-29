d9f4f668478e548b92563c6fd1b41b024f110b49
Sinkyo <yhko1988@gmail.com>
2022-04-29 16:03:21 +0900

    :bug:BUG: Fixing bug by try and catch

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,11 +2,16 @@ import { useState, useEffect } from 'react';
 import { getUserByUid } from '../services/firebase';
 // https://firebase.google.com/docs/auth/admin/manage-users 참고
 function useUser(userId) {
+  console.log(userId);
   const [activeUser, setActiveUser] = useState();
   useEffect(() => {
     async function getUserObjByUserId(userId) {
-      const [user] = await getUserByUid(userId);
-      setActiveUser(user);
+      try {
+        const [user] = await getUserByUid(userId);
+        setActiveUser(user);
+      } catch (error) {
+        setActiveUser([]);
+      }
     }
 
     if (userId) {
