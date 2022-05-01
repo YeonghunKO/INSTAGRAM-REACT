6996ebb6d890bed92c12daffe94121a3bba11fbc
Sinkyo <yhko1988@gmail.com>
2022-05-01 20:26:57 +0900

    :mute::REMOVE LOG: Removing console log

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { getUserByUid } from '../services/firebase';
 // https://firebase.google.com/docs/auth/admin/manage-users 참고
 function useUser(userId) {
-  console.log(userId);
   const [activeUser, setActiveUser] = useState();
   useEffect(() => {
     async function getUserObjByUserId(userId) {
