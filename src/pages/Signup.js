0492cc1409f695c57b7c167c290266204f062f70
Sinkyo <yhko1988@gmail.com>
2022-05-03 22:39:52 +0900

    :iphone::RESPONSIVE:Added instagram pic in login and signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -94,7 +94,7 @@ function Signup() {
   return (
     <div className="container flex flex-col lg:flex-row px-4 lg:px-0 mx-auto max-w-screen-md items-center h-screen">
       {isLoading && <ReactLoader />}
-      <div className="hidden lg:block w-3/5 m-3 ">
+      <div className="block w-3/6 lg:w-3/5 m-3 ">
         <img src="/images/iphone-with-profile.jpg" alt="Iphone Login picture" />
       </div>
       <div className="flex flex-col w-full lg:w-2/5 mt-5">
