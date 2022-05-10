9fefbf0e3f6332dac47a01b4c83b6d1a645a0114
Sinkyo <yhko1988@gmail.com>
2022-05-10 15:14:54 +0900

    :bug::BUG FIX: Set height of img to 100% to stretch the img to parent div size

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
 import ContentLoader from 'react-content-loader';
 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
 function Photos({ photos }) {
   return (
     <>
@@ -15,62 +20,63 @@ function Photos({ photos }) {
             <rect x="260" y="110" rx="5" ry="5" width="120" height="100" />
           </ContentLoader>
         )}
-        <div
-          data-testid="photos"
-          className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-12 px-3 lg:px-0"
-        >
-          {photos?.length
-            ? photos.map((photo, ind) => (
-                <div
-                  data-testid={`photo`}
-                  key={photo.docId}
-                  className={`${
-                    ind + 1 === photos.length && 'mb-6'
-                  } cursor-pointer relative group`}
-                >
-                  <img
-                    className="rounded"
-                    src={photo.imageSrc}
-                    alt={photo.caption}
-                  />
-                  <div className="absolute bottom-0 left-0 bg-gray-base z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex hidden">
-                    <p className="flex items-center text-white font-bold">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="w-8 mr-4"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      {photo.likes.length}
-                    </p>
-                    <p className="flex items-center text-white font-bold">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="w-8 mr-4"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      {photo.comments.length}
-                    </p>
+        {photos?.length ? (
+          <div
+            data-testid="photos"
+            className="grid grid-cols-1 lg:grid-cols-3 gap-2 mb-12 px-3 lg:px-0"
+          >
+            {photos?.length
+              ? photos.map((photo, ind) => (
+                  <div
+                    data-testid={`photo`}
+                    key={photo.docId}
+                    className="h-9/12 mb-3 cursor-pointer relative group"
+                  >
+                    <img
+                      className="rounded h-full"
+                      src={photo.imageSrc}
+                      alt={photo.caption}
+                    />
+                    <div className="absolute bottom-0 left-0 bg-gray-base z-10 w-full justify-evenly items-center h-full bg-black-faded rounded group-hover:flex hidden">
+                      <p className="flex items-center text-white font-bold">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-8 mr-4"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        {photo.likes.length}
+                      </p>
+                      <p className="flex items-center text-white font-bold">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-8 mr-4"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        {photo.comments.length}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))
-            : null}
-        </div>
+                ))
+              : null}
+          </div>
+        ) : null}
+
         {!photos ||
-          (photos.length === 0 && (
+          (photos?.length === 0 && (
             <p className="text-center text-2xl">No Posts Yet</p>
           ))}
       </div>
@@ -83,3 +89,5 @@ export default Photos;
 Photos.propTypes = {
   photos: PropTypes.array,
 };
+
+// ind + 1 === photos.length && 'mb-6'
