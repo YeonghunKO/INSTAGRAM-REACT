afa08ff725255d9f1d6a54436a783dbdbab3ad43
Sinkyo <yhko1988@gmail.com>
2022-05-11 13:54:43 +0900

    :bug::BUG FIX: Fix error related to rules of hooks(order of hooks executing). Moved usePhotos custom hook up to the ProtectedRoute instead of executing inside Dashboard

--- src/helpers/ProtectedRoute.js
+++ src/helpers/ProtectedRoute.js
@@ -2,12 +2,15 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
+import useUser from '../hooks/useUser';
 
 function ProtectedRoute({ user, children }) {
   if (!user) {
     return <Navigate to={ROUTES.LOGIN} replace />;
   }
-  return React.cloneElement(children, { user });
+  const { activeUser } = useUser(user.uid);
+
+  return React.cloneElement(children, { activeUser });
 }
 
 export default ProtectedRoute;
