3b1b644ac662ab0ebbf79aaf28559998ccfd8d9e
Sinkyo <yhko1988@gmail.com>
2022-05-20 12:03:49 +0900

    :fire::REMOVE: Remove unnecessary code

--- src/helpers/ProtectedRoute.js
+++ src/helpers/ProtectedRoute.js
@@ -3,18 +3,17 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
-function ProtectedRoute({ user, activeUser, children }) {
+function ProtectedRoute({ user, children }) {
   if (!user) {
     return <Navigate to={ROUTES.LOGIN} replace />;
   }
 
-  return React.cloneElement(children, { activeUser });
+  return React.cloneElement(children);
 }
 
 export default ProtectedRoute;
 
 ProtectedRoute.propTypes = {
-  activeUser: PropTypes.object,
   user: PropTypes.object,
   children: PropTypes.object.isRequired,
 };
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,25 +1,17 @@
-import PropTypes from 'prop-types';
-
-import { useEffect, useContext } from 'react';
+import { useEffect } from 'react';
 import Header from '../components/Header';
 import Timeline from '../components/Timeline';
 import Sidebar from '../components/sidebar';
 
-import PostPhotosContext from '../context/postPhotos';
-
-
-function Dashboard({ activeUser = {} }) {
-  const { following } = activeUser;
-
-  const { postPhotos, setPostPhotos } = useContext(PostPhotosContext);
+function Dashboard() {
   useEffect(() => {
     document.title = 'Instagram';
   }, []);
   return (
     <div className="bg-gray-background">
-      <Header setPostPhotos={setPostPhotos} />
+      <Header />
       <aside className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg px-4 lg:px-0">
-        <Timeline photos={postPhotos} following={following} />
+        <Timeline />
         <Sidebar />
       </aside>
     </div>
@@ -27,7 +19,3 @@ function Dashboard({ activeUser = {} }) {
 }
 
 export default Dashboard;
-
-Dashboard.propTypes = {
-  activeUser: PropTypes.object,
-};
