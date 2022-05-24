e40571cf4d3e7e1bfab51ad34d7cc3d8c73d662b
Sinkyo <yhko1988@gmail.com>
2022-05-24 16:11:55 +0900

    :construction::WORKING ON: Set up dialogue modal for editing profile

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -7,6 +7,30 @@ import { isUserFollowingProfile, toggleFollow } from '../../services/firebase';
 
 import UserFollowingContext from '../../context/userFollowing';
 
+import { doc, updateDoc } from 'firebase/firestore';
+
+import { getAuth, updateProfile } from 'firebase/auth';
+
+import { db } from '../../lib/firebase';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import AddToPhotosOutlinedIcon from '@mui/icons-material/AddToPhotosOutlined';
+
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import UpgradeIcon from '@mui/icons-material/Upgrade';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Stack from '@mui/material/Stack';
+
+import ReactImageUploading from 'react-images-uploading';
+
+import ReactLoader from '../../components/Loader';
+
 function Header({
   activeUser,
   photosCount,
@@ -15,7 +39,7 @@ function Header({
   profile: {
     docId: profileDocId,
     userId: profileUserId,
-    fullName,
+    fullName: profileFullName,
     followers,
     following,
     photoURL,
@@ -29,10 +53,12 @@ function Header({
 
   const [windowWidth] = useState(window.innerWidth);
 
-  const [profileImg, setProfileImg] = useState(photoURL);
+  const [profileImg, setProfileImg] = useState([]);
   const [username, setUsername] = useState(profileUsername);
-  const [fullname, setFullname] = useState(fullName);
+  const [fullname, setFullname] = useState(profileFullName);
   const [introduction, setIntroduction] = useState(profileIntroduction);
+
+  const [editProfileOpen, setEditProfileOpen] = useState(false);
 
   const activeBtnFollow =
     activeUser?.username && activeUser?.username !== profileUsername;
@@ -66,7 +92,34 @@ function Header({
     }
   };
 
-  const handleEditProfileFollow = async () => {};
+  const handleEditProfileOpen = () => {
+    setEditProfileOpen(true);
+  };
+  const handleEditProfileClose = () => {
+    setEditProfileOpen(false);
+  };
+
+  const onImageChange = (imageList, addUpdateIndex) => {
+    try {
+      if (imageList.length) {
+        setProfileImg([
+          { ...imageList[0], imageName: imageList[0]?.file.name },
+        ]);
+      } else {
+        setProfileImg([]);
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Picture size is too big.');
+    }
+  };
+
+  const handleEditProfileConfirm = async () => {
+    console.log('profileImg', profileImg);
+    console.log('username', username);
+    console.log('fullname', fullname);
+    console.log('introduction', introduction);
+  };
 
   useEffect(() => {
     const isLoggedInUserFollowingProfile = async () => {
@@ -79,9 +132,9 @@ function Header({
     };
     if (activeUser?.username && profileUserId) {
       isLoggedInUserFollowingProfile();
-      setProfileImg(photoURL);
+      setProfileImg([photoURL]);
       setUsername(profileUsername);
-      setFullname(fullName);
+      setFullname(profileFullName);
       setIntroduction(profileIntroduction);
     }
   }, [activeUser?.username, profileUserId]);
@@ -136,10 +189,10 @@ function Header({
               <button
                 className="bg-blue-medium font-bold text-sm rounded text-white w-28 h-10"
                 type="button"
-                onClick={handelToggleFollow}
+                onClick={handleEditProfileOpen}
                 onKeyDown={e => {
                   if (e.key === 'Enter') {
-                    handleEditProfileFollow();
+                    handleEditProfileOpen();
                   }
                 }}
               >
@@ -166,15 +219,135 @@ function Header({
             }
           </div>
           <div className="container mt-4">
-            <p className="font-bold">{fullName}</p>
+            <p className="font-bold">{profileFullName}</p>
           </div>
           <div className="container mt-4">
             <p className="italic">
-              {introduction ? introduction : 'No introduction'}
+              {introduction ? profileIntroduction : 'No introduction'}
             </p>
           </div>
         </div>
       )}
+      <Dialog open={editProfileOpen} onClose={handleEditProfileClose}>
+        <DialogTitle>Edit your profile</DialogTitle>
+        <DialogContent>
+          <p className="text-[1rem] text-[rgba(0,0,0,0.6)]">
+            change your profile image
+          </p>
+          <ReactImageUploading
+            value={profileImg}
+            onChange={onImageChange}
+            dataURLKey="data_url"
+            defaultImg={true}
+          >
+            {({
+              imageList,
+              onImageUpload,
+              onImageUpdate,
+              onImageRemove,
+              isDragging,
+              dragProps,
+            }) => (
+              // write your building UI
+              <div className="upload__image-wrapper">
+                <Button
+                  startIcon={<InsertPhotoIcon />}
+                  style={
+                    isDragging
+                      ? { color: 'red', marginTop: '1rem' }
+                      : { marginTop: '1rem' }
+                  }
+                  onClick={onImageUpload}
+                  {...dragProps}
+                  variant="outlined"
+                  className="my-1"
+                  fullWidth={true}
+                >
+                  {' '}
+                  Click or Drop here
+                </Button>
+                &nbsp;
+                {imageList.map((image, index) => (
+                  <div key={index} className="image-item">
+                    <img
+                      src={`${image.data_url ? image.data_url : profileImg}`}
+                      alt=""
+                      className="w-full"
+                    />
+                    <div className="image-item__btn-wrapper">
+                      <Stack className="mt-3" direction="row" spacing={2}>
+                        <Button
+                          onClick={() => onImageUpdate(index)}
+                          variant="contained"
+                          endIcon={<UpgradeIcon />}
+                        >
+                          Update
+                        </Button>
+                        <Button
+                          onClick={() => onImageRemove(index)}
+                          variant="contained"
+                          color="error"
+                          endIcon={<DeleteForeverIcon />}
+                        >
+                          Remove
+                        </Button>
+                      </Stack>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </ReactImageUploading>
+          <TextField
+            margin="dense"
+            id="username"
+            label="edit your username"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={({ target }) => setUsername(target.value)}
+            value={username}
+          />
+          <TextField
+            margin="dense"
+            id="fullname"
+            label="edit your fullname"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={({ target }) => setFullname(target.value)}
+            value={fullname}
+          />
+          <TextField
+            margin="dense"
+            id="introduction"
+            label="edit your introduction"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={({ target }) => setIntroduction(target.value)}
+            value={introduction}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Stack direction="row" spacing={1}>
+            <Button
+              onClick={handleEditProfileClose}
+              variant="contained"
+              color="error"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={handleEditProfileConfirm}
+              variant="contained"
+              style={{ marginRight: '.3rem' }}
+            >
+              Edit
+            </Button>
+          </Stack>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
