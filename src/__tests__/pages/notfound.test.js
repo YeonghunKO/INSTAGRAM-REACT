b20a24e2868cbf82c8ca97f4022caebd53bf79e1
Sinkyo <yhko1988@gmail.com>
2022-05-28 21:01:09 +0900

    :white_check_mark:TEST: Update notfound test

--- src/__tests__/pages/notfound.test.js
+++ src/__tests__/pages/notfound.test.js
@@ -1,34 +1,55 @@
 import NotFound from '../../pages/NotFound';
 
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import UserContext from '../../context/currentUser';
-
-import loggedInUser from '../../fixtures/logged-in-user';
+import originalPhotosContext from '../../context/originalPost';
+import PostPhotosContext from '../../context/postPhotos';
 
 describe('not found', () => {
   it('renders not found page with logged in user', async () => {
-    const user = { user: { loggedInUser } };
+    const user = {
+      uid: 'uid',
+      displayName: 'sinkyo',
+      photoURL: 'www.naver.com',
+    };
     const { queryByText } = render(
       <Router>
         <UserContext.Provider value={{ user }}>
-          <NotFound />
+          <originalPhotosContext.Provider
+            value={{ originalPhotos: [], setOriginalPhotos: jest.fn() }}
+          >
+            <PostPhotosContext.Provider value={{ setPostPhotos: jest.fn() }}>
+              <NotFound />
+            </PostPhotosContext.Provider>
+          </originalPhotosContext.Provider>
         </UserContext.Provider>
       </Router>
     );
-
     expect(queryByText('Not Found!')).toBeInTheDocument();
     expect(document.title).toBe('Not Foun -Instagram');
     expect(queryByText('Log In')).toBeFalsy();
   });
 
   it('renders not found page without logged in user', async () => {
+    const user = {
+      uid: null,
+      displayName: null,
+      photoURL: null,
+    };
+
     const { queryByText } = render(
       <Router>
-        <UserContext.Provider value={{ user: null }}>
-          <NotFound />
+        <UserContext.Provider value={{ user }}>
+          <originalPhotosContext.Provider
+            value={{ originalPhotos: [], setOriginalPhotos: jest.fn() }}
+          >
+            <PostPhotosContext.Provider value={{ setPostPhotos: jest.fn() }}>
+              <NotFound />
+            </PostPhotosContext.Provider>
+          </originalPhotosContext.Provider>
         </UserContext.Provider>
       </Router>
     );
